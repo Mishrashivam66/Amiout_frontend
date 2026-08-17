1d63ed2d1647698c74ad5adfe15e7397d821b7eb
Shivam Kumar <shivammishra17499@gmail.com>
2026-08-17 19:14:06 +0530

    improved

--- src/services/api.js
+++ src/services/api.js
@@ -10,33 +10,21 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-
-  headers: {
-    "Content-Type": "application/json",
-  },
-
   withCredentials: true,
 });
 
 // ==========================================
 // REQUEST INTERCEPTOR
 // ==========================================
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+  return config;
+});
 
 // ==========================================
 // RESPONSE INTERCEPTOR
